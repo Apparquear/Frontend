60dbbd693283d26fb33b5f256ea39291e72ea4e8
Liseth Yurany Arévalo Yaruro <lyarevalo@unal.edu.co>
2020-10-04 21:03:53 -0500

    Registro Inicial

--- apparquear/src/App.vue
+++ apparquear/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="inicio">
+    <Register msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Register from './components/Register.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Register
   }
 }
 </script>
