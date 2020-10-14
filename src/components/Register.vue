a993b153aa52adf08750662a4ab3b33f14b34a90
Brayan Hurtado <bohurtadop@unal.edu.co>
2020-10-13 22:57:32 -0500

    Ajuste toast

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -177,7 +177,7 @@ export default {
         )
         .then(response => {
           if(response && response.status == 200){
-            this.makeToast("sucess", "Registro completado", "Has completado tu registro exitosamente");
+            this.makeToast("success", "Registro completado", "Has completado tu registro exitosamente");
             setTimeout(function(){
               this.toLogin()
             }.bind(this),1000);
